d279003c18da2c362782e1e70dcf06a726513460
GolferChen <671608295@qq.com>
2023-11-09 17:12:14 +0800

    add LD and imper files

--- gen_csv_1.py
+++ gen_csv_1.py
@@ -1,26 +1,77 @@
 
+# import numpy as np
+# import pandas as pd
+# import os
+
+# url_predix = "https://golferchen.github.io/human-study-8888"
+# info = dict()
+# info["gt_audio_url"] = []
+# info["audio_url"] = []
+
+# root = '1/audio'
+# for pair_flag in sorted(os.listdir(root)):
+#     pair_dir = os.path.join(root, pair_flag)
+#     wav_names = os.listdir(pair_dir)
+#     wav_names = sorted(wav_names, key=lambda x: len(x)) 
+#     url = "{}/{}/{}/{}".format(url_predix, root, pair_flag, wav_names[0])
+#     info["gt_audio_url"].append(url)
+#     url = "{}/{}/{}/{}".format(url_predix, root, pair_flag, wav_names[1])
+#     info["audio_url"].append(url)
+
+# df = pd.DataFrame(info)
+# df.to_csv("1-unshuffle.csv", index=False)
+# print(df)
+# df = df.sample(frac=1)
+# print(df)
+# df.to_csv("1.csv", index=False)
+
+
+
+# import numpy as np
+# import pandas as pd
+# import os
+
+# url_predix = "https://golferchen.github.io/human-study-8888"
+# info = dict()
+# info["gt_audio_url"] = []
+# info["audio_url"] = []
+
+# root = '2/audio'
+# for pair_flag in sorted(os.listdir(root)):
+#     pair_dir = os.path.join(root, pair_flag)
+#     wav_names = os.listdir(pair_dir)
+#     wav_names = sorted(wav_names, key=lambda x: len(x)) 
+#     url = "{}/{}/{}/{}".format(url_predix, root, pair_flag, wav_names[0])
+#     info["gt_audio_url"].append(url)
+#     url = "{}/{}/{}/{}".format(url_predix, root, pair_flag, wav_names[1])
+#     info["audio_url"].append(url)
+
+# df = pd.DataFrame(info)
+# df.to_csv("2-unshuffle.csv", index=False)
+# print(df)
+# df = df.sample(frac=1)
+# print(df)
+# df.to_csv("2.csv", index=False)
+
+
+
+
 import numpy as np
 import pandas as pd
 import os
 
 url_predix = "https://golferchen.github.io/human-study-8888"
 info = dict()
-info["gt_audio_url"] = []
 info["audio_url"] = []
 
-root = '1/audio'
-for pair_flag in sorted(os.listdir(root)):
-    pair_dir = os.path.join(root, pair_flag)
-    wav_names = os.listdir(pair_dir)
-    wav_names = sorted(wav_names, key=lambda x: len(x)) 
-    url = "{}/{}/{}/{}".format(url_predix, root, pair_flag, wav_names[0])
-    info["gt_audio_url"].append(url)
-    url = "{}/{}/{}/{}".format(url_predix, root, pair_flag, wav_names[1])
+root = '3/audio-partial'
+for wav_name in sorted(os.listdir(root)):
+    url = "{}/{}/{}".format(url_predix, root, wav_name)
     info["audio_url"].append(url)
 
 df = pd.DataFrame(info)
-df.to_csv("1-unshuffle.csv", index=False)
+df.to_csv("3-unshuffle.csv", index=False)
 print(df)
 df = df.sample(frac=1)
 print(df)
-df.to_csv("1.csv", index=False)
+df.to_csv("3.csv", index=False)
--- gen_html_index_1.py
+++ gen_html_index_1.py
@@ -1,9 +1,62 @@
 
 
+# import os
+# import pandas as pd
+
+# html_path = "index.html"
+# writer = open(html_path, "w")
+# writer.write("<!DOCTYPE html>" + "\n")
+# writer.write("<html>" + "\n")
+# writer.write("<head>" + "\n")
+# writer.write("    <title>audios</title>" + "\n")
+# writer.write("</head>" + "\n")
+# writer.write("<body>" + "\n")
+
+# root = '1/audio'
+# for pair_flag in sorted(os.listdir(root)):
+#     pair_dir = os.path.join(root, pair_flag)
+#     for wav_name in sorted(os.listdir(pair_dir)):
+#         url = f'{root}/{pair_flag}/{wav_name}'
+#         item = "    <embed src='" + url + "'/>\n"
+#         writer.write(item)
+        
+# writer.write("</body>" + "\n")
+# writer.write("</html>" + "\n")
+# writer.close()
+
+
+
+
+# import os
+# import pandas as pd
+
+# html_path = "index_2.html"
+# writer = open(html_path, "w")
+# writer.write("<!DOCTYPE html>" + "\n")
+# writer.write("<html>" + "\n")
+# writer.write("<head>" + "\n")
+# writer.write("    <title>audios</title>" + "\n")
+# writer.write("</head>" + "\n")
+# writer.write("<body>" + "\n")
+
+# root = '2/audio'
+# for pair_flag in sorted(os.listdir(root)):
+#     pair_dir = os.path.join(root, pair_flag)
+#     for wav_name in sorted(os.listdir(pair_dir)):
+#         url = f'{root}/{pair_flag}/{wav_name}'
+#         item = "    <embed src='" + url + "'/>\n"
+#         writer.write(item)
+        
+# writer.write("</body>" + "\n")
+# writer.write("</html>" + "\n")
+# writer.close()
+
+
+
 import os
 import pandas as pd
 
-html_path = "index.html"
+html_path = "index_3.html"
 writer = open(html_path, "w")
 writer.write("<!DOCTYPE html>" + "\n")
 writer.write("<html>" + "\n")
@@ -12,13 +65,11 @@ writer.write("    <title>audios</title>" + "\n")
 writer.write("</head>" + "\n")
 writer.write("<body>" + "\n")
 
-root = '1/audio'
-for pair_flag in sorted(os.listdir(root)):
-    pair_dir = os.path.join(root, pair_flag)
-    for wav_name in sorted(os.listdir(pair_dir)):
-        url = f'{root}/{pair_flag}/{wav_name}'
-        item = "    <embed src='" + url + "'/>\n"
-        writer.write(item)
+root = '3/audio-partial'
+for name in sorted(os.listdir(root)):
+    url = f'{root}/{name}'
+    item = "    <embed src='" + url + "'/>\n"
+    writer.write(item)
         
 writer.write("</body>" + "\n")
 writer.write("</html>" + "\n")
